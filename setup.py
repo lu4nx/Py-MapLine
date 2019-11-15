a0a17a0ce322f268f765dcc22b1f76d38a0dc4dc
lu4nx <lx@shellcodes.org>
2019-11-15 17:49:54 +0800

    增加安装脚本；独立 Changelog

---
+++ setup.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+from distutils.core import setup
+
+setup(
+    name='mapline',
+    url='https://github.com/1u4nx/Py-MapLine',
+    author='lu4nx',
+    author_email='lx@shellcodes.org',
+    version='1.1',
+    data_files=[('/usr/bin', ["mapline.py"])]
+)
