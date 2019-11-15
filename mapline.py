af6684aaafd42b1b35b2cf42d3968340118592a4
lu4nx <lx@shellcodes.org>
2019-11-15 17:49:21 +0800

    调整代码风格，自测函数兼容 Python3

--- mapline.py
+++ mapline.py
@@ -2,13 +2,6 @@
 # coding=utf-8
 # author: lu4nx <lx@shellcodes.org>
 
-"""
-Changelog:
-
-2013/04/13: 开发线程池
-2016/01/09: Py-MapLine第一个版本
-"""
-
 import sys
 import logging
 import threading
@@ -16,11 +9,9 @@ import traceback
 from optparse import OptionParser
 
 if sys.version_info.major == 2:
-    from Queue import Empty
-    from Queue import Queue
+    from Queue import Empty, Queue
 elif sys.version_info.major == 3:
-    from queue import Empty
-    from queue import Queue
+    from queue import Empty, Queue
 
 
 queue = Queue()
@@ -30,13 +21,10 @@ logging.basicConfig(
     format='%(asctime)s %(threadName)s %(levelname)s %(message)s',
     filemode='w'
 )
-# 打开线程调试息信
-# threading._VERBOSE=True
 
 
 class ThreadWork(threading.Thread):
-    """ 从队列中取得函数以及参数并执行
-    """
+    """从队列中取得函数以及参数并执行"""
 
     def __init__(self, queue):
         threading.Thread.__init__(self)
@@ -56,24 +44,22 @@ class ThreadWork(threading.Thread):
 
 
 class ThreadPool(object):
-    """ 线程池的具体实现
+    """线程池的具体实现
+
+    :param thread_count: 线程池大小（线程数）
     """
 
     def __init__(self, thread_count=10):
-        """ thread_count: 线程池大小（创建的线程数）
-        """
-
         try:
             self.thread_count = int(thread_count)
-        except:
+        except Exception:
             raise SystemExit('thread_count not is a number')
 
         self.work_list = []
         self.init_threadpool()
 
     def init_threadpool(self):
-        """ 初始化线程池，并启动线程
-        """
+        """初始化线程池，并启动线程"""
         self.work_queue = Queue()
 
         for _ in range(self.thread_count):
@@ -84,8 +70,7 @@ class ThreadPool(object):
             t.start()
 
     def putjob(self, work_func, func_args=None):
-        """ 把任务放入线程池调动
-        """
+        """把任务放入线程池调动"""
         self.work_queue.put((work_func, func_args))
 
     def wait(self):
@@ -93,23 +78,20 @@ class ThreadPool(object):
 
 
 def self_test():
-    """ 线程池自测试
-    """
-    q = Queue()
-    urls = ['http://www.baidu.com',
-            'http://www.jiasule.com'] * 10
-
+    """线程池自测试"""
     # 注意必须要指定参数
-    def test(*argv):
-        """ 从队列中取得url并urlopen，然后打印句柄
-        """
-        import urllib2
+    def test_func(*argv):
+        """从队列中取得url并urlopen，然后打印句柄"""
+        if sys.version_info.major == 2:
+            from urllib2 import urlopen
+        else:
+            from urllib.request import urlopen
 
-        while 1:
+        while True:
             try:
                 url = q.get(block=False)
                 try:
-                    sock = urllib2.urlopen(url)
+                    sock = urlopen(url)
                     print(sock)
                     sock.close()
                 except Exception:
@@ -120,12 +102,18 @@ def self_test():
             except Empty:
                 break
 
-    map(lambda url: q.put(url), urls)
+    q = Queue()
+    urls = ['http://www.baidu.com',
+            'http://www.qq.com'] * 10
+
+    for url in urls:
+        q.put(url)
 
     # (1) 创建线程池
     threadpool = ThreadPool(thread_count=10)
     # (2) 将任务放入任务队列
-    map(lambda i: threadpool.putjob(test, func_args="not args"), xrange(3))
+    for i in range(3):
+        threadpool.putjob(test_func, func_args=None)
     # (3) 等待完成队列里所有任务
     threadpool.wait()
 
